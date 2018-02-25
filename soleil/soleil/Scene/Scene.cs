c9b389b0c5a5b7c61f01b6ecf32671ce3a57359b
asRagi <ragi-justaway@hotmail.co.jp>
2018-02-25 23:56:32 +0900

    add Scene and SceneManager

---
+++ soleil/soleil/Scene/Scene.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soleil
+{
+    abstract class Scene
+    {
+
+        bool active,dead;
+        public Scene(SceneManager sm)
+        {
+            active = true;
+            dead = false;
+            sm.Add(this);
+        }
+
+        virtual public void Update()
+        {
+
+        } 
+
+        virtual public void Draw(SpriteBatch sb)
+        {
+
+        }
+
+        public bool isDead()
+        {
+            return dead;
+        }
+
+        public bool isActive()
+        {
+            return active;
+        }
+
+    }
+}
---
+++ soleil/soleil/Scene/SceneManager.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soleil
+{
+    class SceneManager
+    {
+        List<Scene> scenes;
+        public SceneManager(Scene s)
+        {
+            scenes = new List<Scene>();
+            Add(s);
+        }
+
+        public void Add(Scene scene)
+        {
+            scenes.Add(scene);
+        }
+
+        public void Update()
+        {
+            scenes.RemoveAll(s => s.isDead());
+            scenes.FindAll(s => s.isActive());
+        }
+
+        public void Draw(SpriteBatch sb)
+        {
+            scenes.Last().Draw(sb); // いい感じにする
+        }
+    }
+}
