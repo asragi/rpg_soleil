df4d64d2a3ee5b621dfca413c1a3728866e07070
asRagi <ragi-justaway@hotmail.co.jp>
2018-02-26 18:42:20 +0900

    add file that was forgetten to commit

---
+++ soleil/soleil/Map/PlayerObject.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace Soleil
+{
+    class PlayerObject : MapObject
+    {
+        // AnimationSet anim;
+        public PlayerObject(ObjectManager om)
+            : base(om)
+        {
+
+        }
+
+        public override void Update()
+        {
+            base.Update();
+        }
+
+        public override void Draw(Drawing sb)
+        {
+            Console.WriteLine("iikanji");
+            sb.Draw(pos,Resources.GetTexture(TextureID.White),DepthID.Item);
+            base.Draw(sb);
+        }
+    }
+}
