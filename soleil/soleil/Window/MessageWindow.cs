990875282dfa6ce6560d54aae36d709b1d8c5a4e
asRagi <ragi-justaway@hotmail.co.jp>
2018-03-15 09:24:57 +0900

    Add Selectable Window and Message Window

---
+++ soleil/soleil/Window/MessageWindow.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soleil
+{
+    class MessageWindow
+        :SelectableWindow
+    {
+        public MessageWindow(Vector _pos, Vector _size, WindowManager wm)
+            : base(_pos,_size,wm)
+        {
+
+        }
+    }
+}
