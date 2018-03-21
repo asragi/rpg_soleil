3e212687021caa5a412fca378057b975814668a6
taiseiKMC <taisei@kmc.gr.jp>
2018-03-22 02:34:17 +0900

    Add Reference.cs

---
+++ soleil/soleil/Function/Reference.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soleil
+{
+    /// <summary>
+    /// 値型の参照
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    class Reference<T>
+    {
+        T value;
+        public Reference() { }
+        public Reference(T _value) => value = _value;
+        public T Get() => value;
+    }
+}
