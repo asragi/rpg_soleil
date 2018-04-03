63bf72a459d1e0d544d50b63754f26a51617da23
asRagi <ragi.zip@live.jp>
2018-04-04 01:56:42 +0900

    Add MapConstruct Class

---
+++ soleil/soleil/Map/MapConstruct.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soleil
+{
+    /// <summary>
+    /// Map上の描画深度を管理する列挙型.
+    /// </summary>
+    public enum MapDepth
+    {
+        Ground,
+
+        /// <summary>
+        /// プレイヤーのY座標に応じて描画深度を変更.
+        /// </summary>
+        Adjust,
+
+        Top,
+    }
+    /// <summary>
+    /// マップ上の障害物などに関するクラス
+    /// </summary>
+    class MapConstruct : MapObject
+    {
+        const DepthID LowerLayer = DepthID.PlayerBack;
+        const DepthID HigherLayer = DepthID.PlayerFront;
+        Texture2D texture;
+        MapDepth depth;
+        DepthID depthId;
+
+        public MapConstruct(TextureID tex,MapDepth dep,ObjectManager om)
+            : this(Vector.Zero,tex,dep,om){}
+
+        public MapConstruct(Vector _pos, TextureID tex,MapDepth dep, ObjectManager om)
+            :base(om)
+        {
+            pos = _pos;
+            texture = Resources.GetTexture(tex);
+            depth = dep;
+            switch (depth)
+            {
+                case MapDepth.Ground:
+                    depthId = LowerLayer;
+                    break;
+                case MapDepth.Adjust:
+                    depthId = HigherLayer;
+                    break;
+                case MapDepth.Top:
+                    depthId = HigherLayer;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public override void Update()
+        {
+
+            base.Update();
+        }
+
+        public override void Draw(Drawing d)
+        {
+            d.Draw(pos, texture, depthId);
+            base.Draw(d);
+        }
+    }
+}
