befeed9a6f93dd39838243638e566a54d97d62d0
asRagi <ragi-justaway@hotmail.co.jp>
2018-04-04 21:48:01 +0900

    Update Camera to make it behave as Singleton

--- soleil/soleil/Function/Camera.cs
+++ soleil/soleil/Function/Camera.cs
@@ -7,8 +7,19 @@ namespace Soleil
 {
     class Camera
     {
+        // Singleton
+        private static Camera camera = new Camera();
+        private Camera(){}
+        public static Camera GeInstance() => camera;
+
         public Vector Position;
         public Vector Delta;
+        Drawing drawing;
+
+        public void SetDrawing(Drawing d)
+        {
+            drawing = d;
+        }
 
         public void Move(Drawing d)
         {
