22043526df0c045028f424acbdab42ba02616678
asRagi <ragi-justaway@hotmail.co.jp>
2018-04-13 16:41:39 +0900

    Update Transition

--- soleil/soleil/Function/Transition.cs
+++ soleil/soleil/Function/Transition.cs
@@ -8,7 +8,10 @@ namespace Soleil
     class Transition
     {
         const int DefaultTransitionSpeed = 21;
+
         int transitionSpeed;
+        Color transitionColor;
+
         TransitionMode mode;
         Texture2D white;
         Color[] texData;
@@ -19,10 +22,13 @@ namespace Soleil
         public Transition()
         {
             transitionSpeed = DefaultTransitionSpeed;
+            transitionColor = Color.AliceBlue;
             mode = TransitionMode.None;
             rule = Resources.GetTexture(TextureID.Rule1);
             white = Resources.GetTexture(TextureID.WhiteWindow);
             texData = new Color[white.Width * white.Height];
+            alphaData = new int[rule.Width * rule.Height];
+            nowAlpha = new int[rule.Width * rule.Height];
             SetRule();
         }
 
@@ -30,8 +36,6 @@ namespace Soleil
         void SetRule()
         {
             var data = new Color[rule.Width * rule.Height];
-            alphaData = new int[data.Length];
-            nowAlpha = new int[data.Length];
             rule.GetData(data);
             for (int i = 0; i < data.Length; i++)
             {
@@ -47,10 +51,10 @@ namespace Soleil
                 case TransitionMode.None:
                     break;
                 case TransitionMode.FadeOut:
-                    RefreshForFadeOut();
+                    RefreshForFade(mode);
                     break;
                 case TransitionMode.FadeIn:
-                    RefreshForFadeIn();
+                    RefreshForFade(mode);
                     break;
                 default:
                     break;
@@ -72,50 +76,50 @@ namespace Soleil
             }
         }
 
-        private void RefreshForFadeOut()
+        private void RefreshForFade(TransitionMode mode)
         {
+            var addNum = 255; // 初期の透明度に加減算する値
+            if (mode == TransitionMode.FadeOut) addNum *= -1;
             for (int i = 0; i < nowAlpha.Length; i++)
             {
-                nowAlpha[i] = alphaData[i] - 255;
+                nowAlpha[i] = alphaData[i] + addNum;
             }
             white.GetData(texData);
         }
 
         private void FadeOutUpdate()
         {
+            var stop = true;
             for (int i = 0; i < texData.Length; i++)
             {
                 nowAlpha[i] += transitionSpeed;
-                texData[i] = Color.White * (MathEx.Clamp(nowAlpha[i], 255, 0)/255.0f);
+                texData[i] = transitionColor * (MathEx.Clamp(nowAlpha[i], 255, 0)/255.0f);
+                if (nowAlpha[i] < 255) stop = false; // 1pixelでも不透明になりきっていなければ処理を止めない
             }
             white.SetData(texData);
-        }
-
-
-        private void RefreshForFadeIn()
-        {
-            for (int i = 0; i < nowAlpha.Length; i++)
-            {
-                nowAlpha[i] = alphaData[i] + 255;
-            }
-            white.GetData(texData);
+            if (stop) SetMode(TransitionMode.None);
         }
 
         private void FadeInUpdate()
         {
+            var stop = true;
             for (int i = 0; i < texData.Length; i++)
             {
                 nowAlpha[i] -= transitionSpeed;
-                texData[i] = texData[i] * (MathEx.Clamp(nowAlpha[i], 255, 0) / 255.0f);
+                texData[i] = transitionColor * (MathEx.Clamp(nowAlpha[i], 255, 0) / 255.0f);
+                if (nowAlpha[i] >= 0) stop = false; // 1pixelでも透明になりきっていなければ処理を止めない
             }
             white.SetData(texData);
+            if (stop) SetMode(TransitionMode.None);
         }
 
         public void Draw(Drawing d)
         {
             var tmp = d.CenterBased;
             d.CenterBased = false;
+            d.SetDrawAbsolute();
             d.Draw(Vector.Zero, white, DepthID.Debug);
+            d.SetDrawNormal();
             d.CenterBased = tmp;
         }
 
