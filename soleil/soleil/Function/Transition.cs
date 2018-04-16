c1607d2464929db7c7cb74687c773b31e08e66b1
asRagi <ragi-justaway@hotmail.co.jp>
2018-04-16 18:32:46 +0900

    Update Transition

--- soleil/soleil/Function/Transition.cs
+++ soleil/soleil/Function/Transition.cs
@@ -7,40 +7,22 @@ namespace Soleil
 
     class Transition
     {
-        const int DefaultTransitionSpeed = 21;
-
-        int transitionSpeed;
-        Color transitionColor;
-
+        const int TransitionTime = 20; // リソースの枚数と対応するため変更時注意
         TransitionMode mode;
-        Texture2D white;
-        Color[] texData;
-        Texture2D rule;
-        int[] alphaData; // Rule画像からalpha値の配列を生成
-        int[] nowAlpha; // 適用する現在のalpha値
+        Texture2D[] white;
+        int index;
+        bool stop;
 
         public Transition()
         {
-            transitionSpeed = DefaultTransitionSpeed;
-            transitionColor = Color.AliceBlue;
-            mode = TransitionMode.None;
-            rule = Resources.GetTexture(TextureID.Rule1);
-            white = Resources.GetTexture(TextureID.WhiteWindow);
-            texData = new Color[white.Width * white.Height];
-            alphaData = new int[rule.Width * rule.Height];
-            nowAlpha = new int[rule.Width * rule.Height];
-            SetRule();
-        }
-
-        // ここだけで1Fくらいかかりそう
-        void SetRule()
-        {
-            var data = new Color[rule.Width * rule.Height];
-            rule.GetData(data);
-            for (int i = 0; i < data.Length; i++)
+            white = new Texture2D[TransitionTime];
+            for (int i = 0; i < TransitionTime; i++)
             {
-                alphaData[i] = ReturnLuminance(data[i]);
+                white[i] = Resources.GetTexture(TextureID.Rule0 + i);
             }
+            index = 0;
+            stop = true;
+            mode = TransitionMode.None;
         }
 
         public void SetMode(TransitionMode _mode)
@@ -78,47 +60,51 @@ namespace Soleil
 
         private void RefreshForFade(TransitionMode mode)
         {
-            var addNum = 255; // 初期の透明度に加減算する値
-            if (mode == TransitionMode.FadeOut) addNum *= -1;
-            for (int i = 0; i < nowAlpha.Length; i++)
+            switch (mode)
             {
-                nowAlpha[i] = alphaData[i] + addNum;
+                case TransitionMode.FadeIn:
+                    index = 0;
+                    stop = false;
+                    break;
+                case TransitionMode.FadeOut:
+                    index = TransitionTime - 1;
+                    stop = false;
+                    break;
+                default:
+                    System.Console.WriteLine("Transition error");
+                    break;
             }
-            white.GetData(texData);
         }
 
         private void FadeOutUpdate()
         {
-            var stop = true;
-            for (int i = 0; i < texData.Length; i++)
+            if (stop || index <= 0)
             {
-                nowAlpha[i] += transitionSpeed;
-                texData[i] = transitionColor * (MathEx.Clamp(nowAlpha[i], 255, 0)/255.0f);
-                if (nowAlpha[i] < 255) stop = false; // 1pixelでも不透明になりきっていなければ処理を止めない
+                stop = true;
+                SetMode(TransitionMode.None);
+                return;
             }
-            white.SetData(texData);
-            if (stop) SetMode(TransitionMode.None);
+            index--;
         }
 
         private void FadeInUpdate()
         {
-            var stop = true;
-            for (int i = 0; i < texData.Length; i++)
+            if (stop || index >= TransitionTime - 1)
             {
-                nowAlpha[i] -= transitionSpeed;
-                texData[i] = transitionColor * (MathEx.Clamp(nowAlpha[i], 255, 0) / 255.0f);
-                if (nowAlpha[i] >= 0) stop = false; // 1pixelでも透明になりきっていなければ処理を止めない
+                stop = true;
+                SetMode(TransitionMode.None);
+                return;
             }
-            white.SetData(texData);
-            if (stop) SetMode(TransitionMode.None);
+            index++;
         }
 
         public void Draw(Drawing d)
         {
+            if (TransitionMode.None == mode && index == TransitionTime - 1) return;
             var tmp = d.CenterBased;
             d.CenterBased = false;
             d.SetDrawAbsolute();
-            d.Draw(Vector.Zero, white, DepthID.Debug);
+            d.Draw(Vector.Zero, white[index], DepthID.Debug);
             d.SetDrawNormal();
             d.CenterBased = tmp;
         }
