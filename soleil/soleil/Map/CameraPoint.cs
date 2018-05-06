2974e153e2ab8c7c4d617ac00c02eb9e01ab2979
Shuma Ito <asragi@SERVAL.KMC>
2018-05-06 16:17:20 +0900

    Add CameraPoint.cs

---
+++ soleil/soleil/Map/CameraPoint.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soleil.Map
+{
+    class CameraPoint
+    {
+        Vector pos;
+        public CameraPoint(Vector _pos)
+        {
+            pos = _pos;
+        }
+
+        public Vector GetPos() => pos;
+    }
+}
