5e71cd6b6c3a40e03476373f9c44e12ee03d473d
asRagi <ragi-justaway@hotmail.co.jp>
2018-09-09 18:36:41 +0900

    Remove magic number

--- soleil/soleil/Menu/ItemMenu.cs
+++ soleil/soleil/Menu/ItemMenu.cs
@@ -8,18 +8,20 @@ namespace Soleil.Menu
 {
     class ItemMenu : MenuChild
     {
+        readonly Vector WindowPos = new Vector(300, 0);
+        readonly Vector WindowStartPos = new Vector(800, 0);
         Image backImage;
         public ItemMenu(MenuComponent parent)
             :base(parent)
         {
-            backImage = new Image(0, Resources.GetTexture(TextureID.WhiteWindow), new Vector(800, 0), DepthID.MessageBack, false, true, 0);
+            backImage = new Image(0, Resources.GetTexture(TextureID.WhiteWindow), WindowStartPos, DepthID.MessageBack, false, true, 0);
         }
 
         protected override void OnDisable()
         {
             base.OnDisable();
             // Transition Images
-            backImage.MoveTo(new Vector(800, 0), 35, Easing.OutCubic);
+            backImage.MoveTo(WindowStartPos, 35, Easing.OutCubic);
             backImage.Fade(35, Easing.OutCubic, false);
         }
 
@@ -27,7 +29,7 @@ namespace Soleil.Menu
         {
             base.OnEnable();
             // Transition Images
-            backImage.MoveTo(new Vector(300, 0), 35, Easing.OutCubic);
+            backImage.MoveTo(WindowPos, 35, Easing.OutCubic);
             backImage.Fade(35, Easing.OutCubic, true);
         }
 
