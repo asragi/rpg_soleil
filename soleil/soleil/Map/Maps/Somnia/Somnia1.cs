33fc36ad325202c9dba71d7679e1c099313a69b8
asRagi <ragi-justaway@hotmail.co.jp>
2018-09-22 23:30:56 +0900

    Add Somnia1 map

---
+++ soleil/soleil/Map/Maps/Somnia/Somnia1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soleil.Map
+{
+    class Somnia1 : MapBase
+    {
+        public Somnia1()
+            : base(MapName.Somnia1)
+        {
+
+        }
+    }
+}
