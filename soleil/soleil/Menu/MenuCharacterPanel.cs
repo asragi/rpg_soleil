55df43c5eddf6f503e844f2d7276270fe636ea2d
asRagi <ragi-justaway@hotmail.co.jp>
2018-09-22 15:17:34 +0900

    Add MenuCharacterPanel

---
+++ soleil/soleil/Menu/MenuCharacterPanel.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soleil.Menu
+{
+    /// <summary>
+    /// メニューにおけるキャラクターのステータス表示コンポーネント
+    /// </summary>
+    class MenuCharacterPanel
+    {
+        readonly Vector FaceImgPos = new Vector(0, 0);
+        readonly Vector HPPos = new Vector(120, 270);
+        const int SpaceHPMP = 40; // HP表示とMP表示のy方向距離
+        const int SpaceVal = 100; // "HP"とHPvalueのx方向表示距離
+        Vector pos;
+        Image faceImg;
+        FontImage hpText, mpText;
+        FontImage hpNumText, mpNumText;
+        List<FontImage> fontImages;
+
+        readonly Func<double, double, double, double, double> EaseFunc = Easing.OutCubic;
+
+        // ほんとは引数でキャラクターIDとかを渡してデータを参照する感じにしたいよね．
+        public MenuCharacterPanel(Vector _pos, TextureID textureID)
+        {
+            pos = _pos;
+            int hp, mp;
+            if(textureID == TextureID.MenuLune) // DEBUG
+            {
+                hp = 297;
+                mp = 834;
+            }
+            else
+            {
+                hp = 654;
+                mp = 425;
+            }
+            // Images
+            faceImg = new Image(0, Resources.GetTexture(textureID), pos + FaceImgPos, DepthID.MessageBack, false, true, 0);
+            fontImages = new List<FontImage>();
+            // hpmpImg
+            hpText = new FontImage(FontID.Test, pos + HPPos, DepthID.MessageBack, true, 0);
+            fontImages.Add(hpText);
+            mpText = new FontImage(FontID.Test, pos + HPPos + new Vector(0, SpaceHPMP), DepthID.MessageBack, true, 0);
+            fontImages.Add(mpText);
+            hpText.Text = "HP";
+            mpText.Text = "MP";
+            hpNumText = new FontImage(FontID.Test, pos + HPPos + new Vector(SpaceVal,0), DepthID.MessageBack, true, 0);
+            fontImages.Add(hpNumText);
+            mpNumText = new FontImage(FontID.Test, pos + HPPos + new Vector(SpaceVal, SpaceHPMP), DepthID.MessageBack, true, 0);
+            fontImages.Add(mpNumText);
+            hpNumText.Text = hp.ToString();
+            mpNumText.Text = mp.ToString();
+            foreach (var item in fontImages)
+            {
+                item.Color = ColorPalette.DarkBlue;
+            }
+        }
+
+        public void FadeIn()
+        {
+            faceImg.Fade(25, EaseFunc, true);
+            foreach (var textImg in fontImages)
+            {
+                textImg.Fade(25, EaseFunc, true);
+            }
+        }
+
+        public void FadeOut()
+        {
+            faceImg.Fade(25, EaseFunc, false);
+            foreach (var textImg in fontImages)
+            {
+                textImg.Fade(25, EaseFunc, false);
+            }
+        }
+
+        public void Update()
+        {
+            faceImg.Update();
+            foreach (var textImg in fontImages)
+            {
+                textImg.Update();
+            }
+        }
+
+        public void Draw(Drawing d)
+        {
+            faceImg.Draw(d);
+            foreach (var textImg in fontImages)
+            {
+                textImg.Draw(d);
+            }
+        }
+    }
+}
--- soleil/soleil/Menu/StatusMenu.cs
+++ soleil/soleil/Menu/StatusMenu.cs
@@ -8,36 +8,15 @@ namespace Soleil.Menu
 {
     class StatusMenu : MenuChild
     {
-        Image faceImgLune, faceImgSun;
-        FontImage HPLune, HPSun, MPLune, MPSun;
-        List<FontImage> fontImages;
-
+        MenuCharacterPanel[] menuCharacterPanels;
         readonly Func<double, double, double, double, double> EaseFunc = Easing.OutCubic;
 
         public StatusMenu(MenuComponent parent)
             :base(parent)
         {
-            faceImgLune = new Image(0, Resources.GetTexture(TextureID.MenuLune), new Vector(290, 120), DepthID.MessageBack, false, true, 0);
-            faceImgSun = new Image(0, Resources.GetTexture(TextureID.MenuSun), new Vector(540, 120), DepthID.MessageBack, false, true, 0);
-
-            // SetText
-            fontImages = new List<FontImage>();
-            HPLune = new FontImage(FontID.Test, new Vector(500, 370), DepthID.MessageBack, true, 0);
-            HPLune.Text = "300";
-            fontImages.Add(HPLune);
-            HPSun = new FontImage(FontID.Test, new Vector(730, 370), DepthID.MessageBack, true, 0);
-            HPSun.Text = "654";
-            fontImages.Add(HPSun);
-            MPLune = new FontImage(FontID.Test, new Vector(500, 420), DepthID.MessageBack, true, 0);
-            MPLune.Text = "864";
-            fontImages.Add(MPLune);
-            MPSun = new FontImage(FontID.Test, new Vector(730, 420), DepthID.MessageBack, true, 0);
-            MPSun.Text = "425";
-            fontImages.Add(MPSun);
-            foreach (var textImg in fontImages)
-            {
-                textImg.Color = ColorPalette.DarkBlue;
-            }
+            menuCharacterPanels = new MenuCharacterPanel[2];
+            menuCharacterPanels[0] = new MenuCharacterPanel(new Vector(290, 120), TextureID.MenuLune);
+            menuCharacterPanels[1] = new MenuCharacterPanel(new Vector(540, 120), TextureID.MenuSun);
         }
 
         protected override void OnDisable()
@@ -55,11 +34,9 @@ namespace Soleil.Menu
         /// </summary>
         public void FadeIn()
         {
-            faceImgSun.Fade(30, EaseFunc, true);
-            faceImgLune.Fade(30, EaseFunc, true);
-            foreach (var textImg in fontImages)
+            foreach (var panel in menuCharacterPanels)
             {
-                textImg.Fade(30, EaseFunc, true);
+                panel.FadeIn();
             }
         }
 
@@ -68,11 +45,9 @@ namespace Soleil.Menu
         /// </summary>
         public void FadeOut()
         {
-            faceImgSun.Fade(30, EaseFunc, false);
-            faceImgLune.Fade(30, EaseFunc, false);
-            foreach (var textImg in fontImages)
+            foreach (var panel in menuCharacterPanels)
             {
-                textImg.Fade(30, EaseFunc, false);
+                panel.FadeOut();
             }
         }
 
@@ -80,24 +55,20 @@ namespace Soleil.Menu
         {
             base.Update();
 
-            // Image Update
-            faceImgSun.Update();
-            faceImgLune.Update();
-            foreach (var textImg in fontImages)
+            // Panel Update
+            foreach (var panel in menuCharacterPanels)
             {
-                textImg.Update();
+                panel.Update();
             }
         }
 
         public override void Draw(Drawing d)
         {
             base.Draw(d);
-            // Image Draw
-            faceImgSun.Draw(d);
-            faceImgLune.Draw(d);
-            foreach (var textImg in fontImages)
+            // Panel Draw
+            foreach (var panel in menuCharacterPanels)
             {
-                textImg.Draw(d);
+                panel.Draw(d);
             }
         }
 
