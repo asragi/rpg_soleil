3643a045158a1dcd971c82c2efc4882cf9a719c2
asRagi <ragi-justaway@hotmail.co.jp>
2018-09-22 20:45:23 +0900

    Add Magic Window

--- soleil/soleil/Menu/MagicMenu.cs
+++ soleil/soleil/Menu/MagicMenu.cs
@@ -6,8 +6,23 @@ using System.Threading.Tasks;
 
 namespace Soleil.Menu
 {
-    class MagicMenu
+    class MagicMenu : BasicMenu
     {
+        public MagicMenu(MenuComponent parent)
+            : base(parent)
+        {
+            // 実際はキャラクターのデータから生成する
+            Panels = new MagicMenuPanel[]{
+                new MagicMenuPanel("サンダーボルト", 8, this),
+                new MagicMenuPanel("マジカルヒール", 40, this),
+                new MagicMenuPanel("エクスプロード", 16, this),
+                new MagicMenuPanel("ルナティックレイ", 66, this),
+            };
 
+            for (int i = 0; i < Panels.Length; ++i)
+            {
+                Panels[i].LocalPos = ItemDrawStartPos + new Vector(0, (Panels[i].PanelSize.Y + ItemPanelSpacing) * i);
+            }
+        }
     }
 }
---
+++ soleil/soleil/Menu/MagicMenuPanel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soleil.Menu
+{
+    /// <summary>
+    /// アイテムメニューのそれぞれの選択パネルのクラス
+    /// </summary>
+    class MagicMenuPanel : SelectablePanel
+    {
+        public readonly Vector CostPosDiff = new Vector(300, 0);
+        public override Vector LocalPos
+        {
+            get { return _LocalPos; }
+            set
+            {
+                base.LocalPos = value;
+                costImage.Pos = _LocalPos + Spacing + BasicMenu.Pos + CostPosDiff;
+            }
+        }
+
+        // 消費コスト表示
+        int costNum;
+        FontImage costImage;
+
+        public MagicMenuPanel(String itemName, int num, MagicMenu parent)
+            : base(itemName, parent)
+        {
+            // itemNum
+            costNum = num;
+            costImage = new FontImage(ItemFont, LocalPos + parent.Pos + CostPosDiff, DepthID.Message, true, 0);
+            costImage.Color = ColorPalette.DarkBlue;
+            costImage.Text = costNum.ToString();
+
+            LocalPos = Vector.Zero;
+        }
+
+        public override void Fade(int duration, Func<double, double, double, double, double> _easeFunc, bool isFadeIn)
+        {
+            base.Fade(duration, _easeFunc, isFadeIn);
+            costImage.Fade(duration, _easeFunc, isFadeIn);
+        }
+
+        public override void MoveTo(Vector target, int duration, Func<double, double, double, double, double> _easeFunc)
+        {
+            base.MoveTo(target, duration, _easeFunc);
+            costImage.MoveTo(target + CostPosDiff, duration, _easeFunc);
+        }
+
+        public override void Update()
+        {
+            base.Update();
+            costImage.Update();
+        }
+
+        public override void Draw(Drawing d)
+        {
+            base.Draw(d);
+            costImage.Draw(d);
+        }
+    }
+}
