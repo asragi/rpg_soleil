81ebb7d06e2b22f6967437753eb6c1a189ce0f2e
asRagi <ragi-justaway@hotmail.co.jp>
2018-09-22 23:49:01 +0900

    Replace Somnia1 Images

--- soleil/soleil/Map/Maps/Somnia/Somnia1.cs
+++ soleil/soleil/Map/Maps/Somnia/Somnia1.cs
@@ -8,10 +8,29 @@ namespace Soleil.Map
 {
     class Somnia1 : MapBase
     {
+        MapConstruct[] mapConstructs;
+        CameraPoint[] cameraPoints;
         public Somnia1()
             : base(MapName.Somnia1)
         {
-
+            mapConstructs = new MapConstruct[]{
+                new MapConstruct(TextureID.Somnia1_2,MapDepth.Ground,om),
+                new AdjustConstruct(TextureID.Somnia1_3,600,om),
+                new AdjustConstruct(TextureID.Somnia1_4,850,om),
+                new MapConstruct(TextureID.Somnia1_1,MapDepth.Top,om),
+                new AdjustConstruct(TextureID.Somnia1_5,600,om),
+            };
+            // マップサイズの設定
+            MapCameraManager.SetMapSize(1260, 886);
+            // CameraPointの設定
+            cameraPoints = new CameraPoint[] {
+                new CameraPoint(868,771),
+                new CameraPoint(1381,1140),
+                new CameraPoint(237,587),
+                new CameraPoint(1312,282),
+                new CameraPoint(620,440),
+            };
+            MapCameraManager.SetCameraPoint(cameraPoints);
         }
     }
 }
