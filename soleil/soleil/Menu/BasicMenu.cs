3d90c705415dca6a93ac8d5c9894456e7cdb7c61
asRagi <ragi-justaway@hotmail.co.jp>
2018-09-22 20:38:47 +0900

    Refactoring Menu Windows system

---
+++ soleil/soleil/Menu/BasicMenu.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soleil.Menu
+{
+    /// <summary>
+    /// メニュー画面で登場する小さなウィンドウの基底
+    /// </summary>
+    abstract class BasicMenu : MenuChild
+    {
+        const int FadeSpeed = 30; // アイテムメニューが出現するスピード
+        const int RowSize = 8; // 現在のフォントサイズだと8項目がちょうどよい
+        protected readonly Vector WindowPos = new Vector(330, 100);
+        protected readonly Vector WindowStartPos = new Vector(830, 100);
+
+        protected readonly Vector ItemDrawStartPos = new Vector(25, 28);
+        protected readonly int ItemPanelSpacing = 4;
+
+        Image backImage;
+        public Vector Pos { get { return backImage.Pos; } }
+        protected int Index;
+        protected SelectablePanel[] Panels;
+
+        public BasicMenu(MenuComponent parent)
+            : base(parent)
+        {
+            backImage = new Image(0, Resources.GetTexture(TextureID.MenuModalBack), WindowStartPos, DepthID.MessageBack, false, true, 0);
+            Index = 0;
+        }
+
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+            // Transition Images
+            backImage.MoveTo(WindowStartPos, FadeSpeed, MenuSystem.EaseFunc);
+            backImage.Fade(FadeSpeed, MenuSystem.EaseFunc, false);
+
+            foreach (var item in Panels)
+            {
+                item.MoveTo(WindowStartPos + item.LocalPos, FadeSpeed, MenuSystem.EaseFunc);
+                item.Fade(FadeSpeed, MenuSystem.EaseFunc, false);
+            }
+        }
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            // Transition Images
+            backImage.MoveTo(WindowPos, FadeSpeed, MenuSystem.EaseFunc);
+            backImage.Fade(FadeSpeed, MenuSystem.EaseFunc, true);
+            foreach (var item in Panels)
+            {
+                item.MoveTo(WindowPos + item.LocalPos, FadeSpeed, MenuSystem.EaseFunc);
+                item.Fade(FadeSpeed, MenuSystem.EaseFunc, true);
+            }
+            RefreshSelected();
+        }
+
+        public override void Update()
+        {
+            base.Update();
+            backImage.Update();
+            foreach (var item in Panels)
+            {
+                item.Update();
+            }
+        }
+
+        public override void Draw(Drawing d)
+        {
+            base.Draw(d);
+            backImage.Draw(d);
+            foreach (var item in Panels)
+            {
+                item.Draw(d);
+            }
+        }
+
+        private void RefreshSelected()
+        {
+            for (int i = 0; i < Panels.Length; i++)
+            {
+                Panels[i].SetSelectedAndFade(i == Index);
+            }
+        }
+
+        // Input
+        public override void OnInputRight() { }
+        public override void OnInputLeft() { }
+
+        public override void OnInputUp()
+        {
+            Index = (Index - 1 + Panels.Length) % Panels.Length;
+            RefreshSelected();
+        }
+
+        public override void OnInputDown()
+        {
+            Index = (Index + 1 + Panels.Length) % Panels.Length;
+            RefreshSelected();
+        }
+        public override void OnInputSubmit() { }
+        public override void OnInputCancel() { Quit(); }
+    }
+}
--- soleil/soleil/Menu/ItemPanel.cs
+++ soleil/soleil/Menu/ItemPanel.cs
@@ -9,112 +9,56 @@ namespace Soleil.Menu
     /// <summary>
     /// アイテムメニューのそれぞれの選択パネルのクラス
     /// </summary>
-    class ItemPanel
+    class ItemPanel : SelectablePanel
     {
-        public readonly Vector PanelSize = new Vector(300, 36);
         public readonly Vector ItemNumPosDiff = new Vector(300, 0);
-        /// <summary>
-        /// パネルの文字描画を左上からどれだけの位置にするか
-        /// </summary>
-        readonly Vector Spacing = new Vector(8, 4);
-        readonly FontID ItemFont = FontID.Test;
-        readonly Func<double, double, double, double, double> func = Easing.OutCubic;
-
-        // Parentに対する相対的な基準座標
-        private Vector localPos;
-        public Vector LocalPos
+        public override Vector LocalPos
         {
-            get { return localPos; }
+            get { return _LocalPos; }
             set
             {
-                localPos = value;
-                itemNumImage.Pos = localPos + Spacing + itemMenu.Pos + ItemNumPosDiff;
-                itemNameImage.Pos = localPos + Spacing + itemMenu.Pos;
-                selectedBack.Pos = localPos + itemMenu.Pos;
+                base.LocalPos = value;
+                itemNumImage.Pos = _LocalPos + Spacing + BasicMenu.Pos + ItemNumPosDiff;
             }
         }
-        ItemMenu itemMenu;
-
-        // アイテム名の描画
-        FontImage itemNameImage;
-        readonly public String ItemName;
 
         // 当該アイテムの所持数
         int itemNum;
         FontImage itemNumImage;
 
-        // 選択状態の背景（これCursorとしてくらすにしたほうがよいきがする）
-        Image selectedBack;
-        Image unselectedBack;
-        bool isSelected;
-
         public ItemPanel(String itemName, int num, ItemMenu parent)
+            :base(itemName, parent)
         {
-            ItemName = itemName;
-            itemMenu = parent;
             // itemNum
             itemNum = num;
             itemNumImage = new FontImage(ItemFont, LocalPos+parent.Pos + ItemNumPosDiff, DepthID.Message, true, 0);
             itemNumImage.Color = ColorPalette.DarkBlue;
             itemNumImage.Text = itemNum.ToString();
-            // Set Font Image
-            itemNameImage = new FontImage(ItemFont, LocalPos+parent.Pos, DepthID.Message, true, 0);
-            itemNameImage.Color = ColorPalette.DarkBlue;
-            itemNameImage.Text = itemName;
-
-            // 選択状態を示すやつ
-            selectedBack = new Image(0, Resources.GetTexture(TextureID.MenuSelected), LocalPos + parent.Pos, DepthID.Message, false, true, 0);
-            unselectedBack = new Image(0, Resources.GetTexture(TextureID.MenuUnselected), Vector.Zero, DepthID.Message, false, true, 0);
 
             LocalPos = Vector.Zero;
         }
 
-        public void Fade(int duration, Func<double, double, double, double, double> _easeFunc, bool isFadeIn)
+        public override void Fade(int duration, Func<double, double, double, double, double> _easeFunc, bool isFadeIn)
         {
-            itemNameImage.Fade(duration, _easeFunc, isFadeIn);
+            base.Fade(duration, _easeFunc, isFadeIn);
             itemNumImage.Fade(duration, _easeFunc, isFadeIn);
-            if(isSelected) selectedBack.Fade(duration, _easeFunc, isFadeIn);
-            unselectedBack.Fade(duration, _easeFunc, isFadeIn);
         }
 
-        public void MoveTo(Vector target, int duration, Func<double, double, double, double, double> _easeFunc)
+        public override void MoveTo(Vector target, int duration, Func<double, double, double, double, double> _easeFunc)
         {
-            itemNameImage.MoveTo(target + Spacing, duration, _easeFunc);
+            base.MoveTo(target, duration, _easeFunc);
             itemNumImage.MoveTo(target + ItemNumPosDiff, duration, _easeFunc);
-            selectedBack.MoveTo(target, duration, _easeFunc);
-            unselectedBack.MoveTo(target, duration, _easeFunc);
         }
 
-        public void SetSelectedAndFade(bool select)
+        public override void Update()
         {
-            if (select)
-            {
-                selectedBack.Fade(20, func, true);
-                unselectedBack.Fade(20, func, false);
-                itemNameImage.Color = ColorPalette.AliceBlue;
-            }
-            else if(!select && isSelected) // 選択が解除された瞬間
-            {
-                selectedBack.Fade(20, func, false);
-                unselectedBack.Fade(20, func, true);
-                itemNameImage.Color = ColorPalette.DarkBlue;
-            }
-            isSelected = select;
-        }
-
-        public void Update()
-        {
-            itemNameImage.Update();
+            base.Update();
             itemNumImage.Update();
-            selectedBack.Update();
-            unselectedBack.Update();
         }
 
-        public void Draw(Drawing d)
+        public override void Draw(Drawing d)
         {
-            selectedBack.Draw(d);
-            //unselectedBack.Draw(d);
-            itemNameImage.Draw(d);
+            base.Draw(d);
             itemNumImage.Draw(d);
         }
     }
