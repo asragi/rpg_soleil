b188a6db4a6f5abaf5edebffa0e438a961572b33
asRagi <ragi-justaway@hotmail.co.jp>
2018-09-29 01:00:16 +0900

    update MapBase

--- soleil/soleil/Map/Maps/Somnia/Somnia1.cs
+++ soleil/soleil/Map/Maps/Somnia/Somnia1.cs
@@ -9,9 +9,6 @@ namespace Soleil.Map
 {
     class Somnia1 : MapBase
     {
-        MapConstruct[] mapConstructs;
-        CameraPoint[] cameraPoints;
-
         // Objects
         Cigerman cigerman;
 
@@ -20,7 +17,7 @@ namespace Soleil.Map
         public Somnia1()
             : base(MapName.Somnia1)
         {
-            mapConstructs = new MapConstruct[]{
+            MapConstructs = new MapConstruct[]{
                 new MapConstruct(TextureID.Somnia1_2,MapDepth.Ground,om),
                 new AdjustConstruct(TextureID.Somnia1_5,470,om),
                 new AdjustConstruct(TextureID.Somnia1_4,570,om),
@@ -30,14 +27,14 @@ namespace Soleil.Map
             // マップサイズの設定
             MapCameraManager.SetMapSize(1260, 886);
             // CameraPointの設定
-            cameraPoints = new CameraPoint[] {
+            CameraPoints = new CameraPoint[] {
                 new CameraPoint(400, 300), // hotel entrance
                 new CameraPoint(650, 555), // center
                 new CameraPoint(1100, 840), // right down
                 new CameraPoint(140, 455), // left
                 new CameraPoint(880, 260),
             };
-            MapCameraManager.SetCameraPoint(cameraPoints);
+            MapCameraManager.SetCameraPoint(CameraPoints);
 
             // Objects
             cigerman = new Cigerman(new Vector(750, 600), om, bm);
