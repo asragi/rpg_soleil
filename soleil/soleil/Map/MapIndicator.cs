7afdd7f67833016c4637861d0f5b668acf98d4e5
asRagi <ragi-justaway@hotmail.co.jp>
2018-09-29 18:56:01 +0900

    Add Image Base Class

--- soleil/soleil/Map/MapIndicator.cs
+++ soleil/soleil/Map/MapIndicator.cs
@@ -11,24 +11,24 @@ namespace Soleil.Map
     {
         static Vector upperRight = new Vector(Game1.VirtualWindowSizeX, 0);
 
-        ImageManager imageManager;
+        //ImageManager imageManager;
         int backImageID;
         public MapIndicator()
         {
-            imageManager = new ImageManager();
-            backImageID = imageManager.Create(TextureID.IndicatorBack, upperRight + new Vector(-10,0), DepthID.Frame);
-            imageManager.Get(backImageID).Alpha = 0.5f;
+            //imageManager = new ImageManager();
+            //backImageID = imageManager.Create(TextureID.IndicatorBack, upperRight + new Vector(-10,0), DepthID.Frame);
+            //imageManager.Get(backImageID).Alpha = 0.5f;
         }
 
         public void Update()
         {
-            imageManager.Get(backImageID).Angle += 0.002f;
-            imageManager.Update();
+            //imageManager.Get(backImageID).Angle += 0.002f;
+            //imageManager.Update();
         }
 
         public void Draw(Drawing d)
         {
-            imageManager.Draw(d);
+            //imageManager.Draw(d);
             var textPos = upperRight;
             d.DrawStaticText(textPos + new Vector(-65, 30), Resources.GetFont(FontID.Test), "Day 4", Color.AliceBlue, DepthID.Frame, Vector.One);
             d.DrawStaticText(textPos + new Vector(-30, 70), Resources.GetFont(FontID.Test), "朝", Color.AliceBlue, DepthID.Frame, Vector.One);
