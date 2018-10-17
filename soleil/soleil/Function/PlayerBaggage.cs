42825bf2a51f15b7ed31657bf6f9a8122b23203b
asRagi <ragi-justaway@hotmail.co.jp>
2018-10-17 22:18:45 +0900

    Add PlayerBaggage class

---
+++ soleil/soleil/Function/PlayerBaggage.cs
@@ -0,0 +1,26 @@
+﻿using Soleil.Item;
+using Soleil.Map;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soleil
+{
+    /// <summary>
+    /// 所持金・アイテムなどをまとめたクラス．ゲーム開始の度に廃棄・生成する．
+    /// Sceneを跨いで参照する．
+    /// </summary>
+    class PlayerBaggage
+    {
+        public ItemList Items;
+        public MoneyWallet MoneyWallet;
+
+        public PlayerBaggage()
+        {
+            Items = new ItemList();
+            MoneyWallet = new MoneyWallet();
+        }
+    }
+}
