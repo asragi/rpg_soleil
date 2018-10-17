475b84d91a34db32e796dc229f067fb590efd3a5
asRagi <ragi-justaway@hotmail.co.jp>
2018-10-17 22:07:51 +0900

    Add MoneyWallet class

---
+++ soleil/soleil/Map/MoneyWallet.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soleil.Map
+{
+    /// <summary>
+    /// 所持金を保持するクラス．
+    /// </summary>
+    class MoneyWallet
+    {
+        private int val;
+        public int Val { get { return val; } private set { val = value; } }
+
+        public MoneyWallet()
+        {
+            // Debug
+            val = 50000;
+        }
+
+        /// <summary>
+        /// 所持金を増やす．
+        /// </summary>
+        public void Add(int num) => val += num;
+
+        /// <summary>
+        /// 指定された金額を所持しているかどうかを返す．
+        /// </summary>
+        public bool HasEnough(int num) => val >= num;
+
+        /// <summary>
+        /// 所持金を減らす．足りない場合は減らしてfalseを返す．
+        /// 足りない場合に減らす処理を行う場合はほぼないと思うので，必ずHasEnoughで確認したい．
+        /// </summary>
+        /// <returns>お金が足りたかどうか．</returns>
+        public bool Consume(int num)
+        {
+            var enough = val >= num;
+            val = enough ? val - num : 0;
+            return enough;
+        }
+    }
+}
