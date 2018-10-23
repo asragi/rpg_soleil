07275bb4f0bd0695ba3f4272b5f629970fdf9144
asRagi <ragi-justaway@hotmail.co.jp>
2018-10-24 04:52:29 +0900

    Add DescriptionWindow and MoneyComponent

---
+++ soleil/soleil/Menu/DescriptionWindow.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soleil.Menu
+{
+    class DescriptionWindow
+    {
+        Image windowBack;
+        MenuDescription description;
+        /// <summary>
+        /// ウィンドウつきMenuDescriptionコンポーネント．
+        /// </summary>
+        public DescriptionWindow()
+        {
+
+        }
+    }
+}
---
+++ soleil/soleil/Menu/MoneyComponent.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soleil.Menu
+{
+    using EFunc = Func<double, double, double, double, double>;
+    /// <summary>
+    /// 所持金表示コンポーネント．
+    /// </summary>
+    class MoneyComponent
+    {
+        FontImage text;
+
+        public MoneyComponent(Vector _pos)
+        {
+            text = new FontImage(FontID.Test, _pos, DepthID.Message, true, 0);
+            text.Color = ColorPalette.DarkBlue;
+            text.EnableShadow = true;
+            text.ShadowPos = new Vector(3, 3);
+            text.ShadowColor = ColorPalette.GlayBlue;
+        }
+
+        public void Call()
+        {
+
+        }
+
+        public void Quit()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw(Drawing d)
+        {
+
+        }
+    }
+}
