66473f4414158ce177b17616ec0dfa8ed6f74c9d
asRagi <ragi-justaway@hotmail.co.jp>
2018-10-27 06:35:19 +0900

    属性に対応する日本語を返す関数の追加

--- soleil/soleil/Battle/Action/Attack.cs
+++ soleil/soleil/Battle/Action/Attack.cs
@@ -18,6 +18,21 @@ namespace Soleil
         Electro,
     }
 
+    static class ExtendAttackAttribution
+    {
+        static readonly Dictionary<AttackAttribution, String> dict = new Dictionary<AttackAttribution, string>
+        {
+            {AttackAttribution.Beat, "打撃"},
+            {AttackAttribution.Cut, "斬撃"},
+            {AttackAttribution.Thrust, "弾丸"},
+            {AttackAttribution.Fever, "熱"},
+            {AttackAttribution.Ice, "冷気"},
+            {AttackAttribution.Electro, "電撃"},
+        };
+
+        public static String Name(this AttackAttribution att) => dict[att];
+    }
+
     class Attack : Action
     {
         protected AttackFunc AFunc;
