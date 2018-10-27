21185cc5ada1a1d2c4886eeee962eae48c39bf6f
asRagi <ragi-justaway@hotmail.co.jp>
2018-10-28 00:13:41 +0900

    BackBarコンポーネントの追加

--- soleil/soleil/Images/BackBarImage.cs
+++ soleil/soleil/Images/BackBarImage.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,16 +12,28 @@ namespace Soleil.Images
     /// </summary>
     class BackBarImage
     {
+        const int EdgeSize = 36;
         public Vector Pos { get; set; }
         Image[] images;
 
         public BackBarImage(Vector _pos, int _length, bool centerBased)
         {
             Pos = _pos;
-            images = new[]
+            images = new Image[3];
+            var tex = Resources.GetTexture(TextureID.BackBar);
+            for (int i = 0; i < images.Length; i++)
             {
-                new Image(0, Resources.GetTexture(TextureID.BackBar), Pos, DepthID.MessageBack, false, true, 0),
-            };
+                images[i] = new Image(0, tex, Pos, DepthID.Debug, false, true, 1);
+            }
+            images[0].Rectangle = new Rectangle(0, 0, EdgeSize, tex.Height);
+            images[1].Rectangle = new Rectangle(EdgeSize, 0, tex.Width - 2 * EdgeSize, tex.Height);
+            images[2].Rectangle = new Rectangle(tex.Width - EdgeSize, 0, EdgeSize, tex.Height);
+
+            var size = (_length - 2 * EdgeSize) / (float)(tex.Width - 2 * EdgeSize);
+            images[1].Size = new Vector(size, 1);
+            images[0].Pos = Pos;
+            images[1].Pos = Pos + new Vector(EdgeSize, 0);
+            images[2].Pos = Pos + new Vector(EdgeSize + (_length - 2 * EdgeSize), 0);
         }
 
         public void Update()
