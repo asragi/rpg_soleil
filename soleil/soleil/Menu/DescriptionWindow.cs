95bf5c64b06592774e0550e911a8ecff6cf0c14f
asRagi <ragi-justaway@hotmail.co.jp>
2018-10-29 20:29:11 +0900

    Update DescriptionWindow

--- soleil/soleil/Menu/DescriptionWindow.cs
+++ soleil/soleil/Menu/DescriptionWindow.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Soleil.Images;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,14 +9,32 @@ namespace Soleil.Menu
 {
     class DescriptionWindow
     {
-        Image windowBack;
+        const int Y = 80;
+        const int X = 40;
+        BackBarImage backBar;
         MenuDescription description;
+        public String Text { set => description.Text = value; }
+
         /// <summary>
         /// ウィンドウつきMenuDescriptionコンポーネント．
         /// </summary>
         public DescriptionWindow()
         {
+            var barWidth = Game1.VirtualWindowSizeX - 2 * X;
+            backBar = new BackBarImage(new Vector(X, Y), barWidth, false);
+            description = new MenuDescription(new Vector(X, Y));
+        }
 
+        public void Update()
+        {
+            backBar.Update();
+            description.Update();
+        }
+
+        public void Draw(Drawing d)
+        {
+            backBar.Draw(d);
+            description.Draw(d);
         }
     }
 }
