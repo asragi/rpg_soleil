be9111443170b1f9f1f5371a0025291c2f0df3a2
asRagi <ragi-justaway@hotmail.co.jp>
2018-11-03 08:29:04 +0900

    CallとQuitだけで管理できるImageクラス追加

---
+++ soleil/soleil/Images/UIImage.cs
@@ -0,0 +1,31 @@
+﻿using Soleil.Menu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soleil
+{
+    class UIImage : Image
+    {
+        Vector posDiff;
+        public UIImage(TextureID tex, Vector pos,Vector _posDiff, DepthID dep, bool centerOrigin = false, bool isStatic = true, float alpha = 0)
+            :base(0,Resources.GetTexture(tex),pos,dep,centerOrigin,isStatic,alpha)
+        {
+            posDiff = _posDiff;
+        }
+
+        public void Call()
+        {
+            Fade(MenuSystem.FadeSpeed, MenuSystem.EaseFunc, true);
+            MoveTo(Pos, MenuSystem.FadeSpeed, MenuSystem.EaseFunc);
+        }
+
+        public void Quit()
+        {
+            Fade(MenuSystem.FadeSpeed, MenuSystem.EaseFunc, false);
+            MoveTo(Pos + posDiff, MenuSystem.FadeSpeed, MenuSystem.EaseFunc);
+        }
+    }
+}
