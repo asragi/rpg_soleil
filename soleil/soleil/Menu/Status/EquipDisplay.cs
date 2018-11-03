925fba2d04d9cc966d95fc9b484df739f94e58e8
asRagi <ragi-justaway@hotmail.co.jp>
2018-11-04 04:33:43 +0900

    装備表示

---
+++ soleil/soleil/Menu/Status/EquipDisplay.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soleil.Menu.Status
+{
+    class EquipDisplay : MenuComponent
+    {
+        const int DiffY = 28;
+        FontImage[] texts;
+
+        const string Empty = "------";
+        readonly string[] tmp = new[] {"シルバーワンド", "指定服", "ビーズのアクセサリー", "太陽の髪留め", Empty };
+
+        public EquipDisplay(Vector pos)
+        {
+            texts = new FontImage[5];
+            for (int i = 0; i < texts.Length; i++)
+            {
+                texts[i] = new FontImage(FontID.KkBlack, pos + new Vector(0, DiffY * i), DepthID.MenuMiddle);
+                texts[i].Text = tmp[i];
+            }
+            Components = texts;
+        }
+    }
+}
