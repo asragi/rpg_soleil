22086fd98478b9d8731be1db89506d993dbf7828
asRagi <ragi-justaway@hotmail.co.jp>
2018-11-04 03:45:47 +0900

    属性耐性の追加

--- soleil/soleil/Menu/Status/StatusParamsDisplay.cs
+++ soleil/soleil/Menu/Status/StatusParamsDisplay.cs
@@ -38,8 +38,6 @@ namespace Soleil.Menu.Status
             for (int i = 0; i < texts.Length; i++)
             {
                 texts[i] = new TextWithVal(FontID.Touhaba, pos + new Vector(0,DiffY*i), 130, Words[i], Para[i]);
-                texts[i].TextColor = ColorPalette.DarkBlue;
-                texts[i].ValColor = ColorPalette.DarkBlue;
             }
             Components = texts;
         }
