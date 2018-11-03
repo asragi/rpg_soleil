5bfc182b35302b1f8534e5dd5ff8b0a89511438a
asRagi <ragi-justaway@hotmail.co.jp>
2018-11-03 10:47:52 +0900

    魔法系統に関するステータスクラスの実装

---
+++ soleil/soleil/Menu/Status/StatusMagicCategory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soleil.Menu.Status
+{
+    class StatusMagicCategory : MenuComponent
+    {
+        public StatusMagicCategory(Vector pos)
+            :base()
+        {
+            
+        }
+    }
+}
