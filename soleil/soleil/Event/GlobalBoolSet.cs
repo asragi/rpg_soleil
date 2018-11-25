18e945cdbc3448e26b60c9939b0f42b81a097c38
asRagi <ragi-justaway@hotmail.co.jp>
2018-11-25 23:21:21 +0900

    Globalで揮発しないboolsetクラス

---
+++ soleil/soleil/Event/GlobalBoolSet.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soleil.Event
+{
+    enum BoolEnum
+    {
+        // Somnia
+        MeetAccessary, // アクセサリー売りに会ったことがある．
+        size,
+    }
+    /// <summary>
+    /// 揮発しない，あらゆる場面で共有するboolのリスト．
+    /// </summary>
+    class GlobalBoolSet
+    {
+        BoolSet boolSet;
+        public bool this[BoolEnum i]
+        {
+            set => boolSet[(int)i] = value;
+            get => boolSet[(int)i];
+        }
+
+        public GlobalBoolSet()
+        {
+            boolSet = new BoolSet((int)BoolEnum.size);
+        }
+    }
+}
