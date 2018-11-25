a9744bd4e2df3b85517ed88f39eb631b19b7b6d8
asRagi <ragi-justaway@hotmail.co.jp>
2018-11-25 22:10:06 +0900

    status controller

---
+++ soleil/soleil/Menu/StatusController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soleil.Menu
+{
+    class StatusController
+    {
+        StatusMenu statusMenu;
+        public StatusController(StatusMenu st)
+        {
+            statusMenu = st;
+        }
+    }
+}
