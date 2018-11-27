fc538e407b8b5940e89d98a25975c8bae2e2011d
asRagi <ragi-justaway@hotmail.co.jp>
2018-11-27 22:27:04 +0900

    Characterを動かすイベント

---
+++ soleil/soleil/Event/CharacterMoveEvent.cs
@@ -0,0 +1,31 @@
+﻿using Soleil.Map;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soleil.Event
+{
+    class CharacterMoveEvent : EventBase
+    {
+        WalkCharacter target;
+        Direction dir;
+        int duration, frame;
+        bool waitEnd;
+        public CharacterMoveEvent(WalkCharacter _target, Direction _dir, int _duration, bool _waitEnd = true)
+        {
+            frame = 0;
+            (target, dir, duration, waitEnd) = (_target, _dir, _duration, _waitEnd);
+        }
+
+        public override void Execute()
+        {
+            base.Execute();
+            frame++;
+            // target.Move(dir);
+            if (frame >= duration) Next();
+        }
+
+    }
+}
