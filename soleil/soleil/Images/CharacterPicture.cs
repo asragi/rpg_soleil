6a17527951ea12ceb9502088bee5cdfde18f9a4c
asRagi <ragi-justaway@hotmail.co.jp>
2019-03-01 01:12:41 +0900

    Add CharacterPicture

---
+++ soleil/soleil/Images/CharacterPicture.cs
@@ -0,0 +1,54 @@
+﻿using Soleil.Menu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soleil
+{
+    class CharacterPicture: IComponent
+    {
+        public static DepthID Depth = DepthID.PlayerFront;
+        public bool IsVisible;
+        public bool IsDark;
+
+        UIImage image;
+        UIImage grayImage;
+        UIImage[] images;
+
+        public CharacterPicture(TextureID id, Vector pos, Vector? pos_diff, bool isDark)
+        {
+            IsDark = isDark;
+            image = new UIImage(id, pos, pos_diff, Depth);
+            grayImage = new UIImage(id, pos, pos_diff, Depth);
+        }
+
+        public void Call()
+        {
+            image.Call();
+            grayImage.Call();
+        }
+
+        public void Quit()
+        {
+            image.Quit();
+            grayImage.Quit();
+        }
+
+        public void Update()
+        {
+            image.Update();
+            grayImage.Update();
+        }
+
+        public void Draw(Drawing d)
+        {
+            image.Draw(d);
+            if (!IsDark) return;
+            grayImage.Draw(d);
+        }
+
+
+    }
+}
