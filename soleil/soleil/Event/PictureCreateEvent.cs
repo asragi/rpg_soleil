912d88ae844156fe785dfca3a3856f687a9b3ff4
asRagi <ragi-justaway@hotmail.co.jp>
2019-03-02 01:03:44 +0900

    立ち絵イベント

---
+++ soleil/soleil/Event/PictureCreateEvent.cs
@@ -0,0 +1,36 @@
+﻿using Soleil.Misc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soleil.Event
+{
+    /// <summary>
+    /// 画面にキャラクターの立ち絵を表示する
+    /// </summary>
+    class PictureCreateEvent: EventBase
+    {
+        CharaName name;
+        int position;
+        CharacterPictureHolder holder;
+
+        public PictureCreateEvent(CharaName _name, int _position, CharacterPictureHolder _holder)
+        {
+            (name, position, holder) = (_name, _position, _holder);
+        }
+
+        public override void Start()
+        {
+            base.Start();
+            holder.Create(name, position);
+        }
+
+        public override void Execute()
+        {
+            base.Execute();
+            Next();
+        }
+    }
+}
