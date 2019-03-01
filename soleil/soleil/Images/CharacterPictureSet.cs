3116b9a207492a2eb645d3cbe0dd9d4ac9c97f76
asRagi <ragi-justaway@hotmail.co.jp>
2019-03-01 19:32:15 +0900

    update character picture set

--- soleil/soleil/Images/CharacterPictureSet.cs
+++ soleil/soleil/Images/CharacterPictureSet.cs
@@ -1,4 +1,5 @@
-﻿using Soleil.Misc;
+﻿using Soleil.Menu;
+using Soleil.Misc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,7 +18,7 @@ namespace Soleil
     /// <summary>
     /// キャラクターごとの立ち絵のセット
     /// </summary>
-    class CharacterPictureSet
+    class CharacterPictureSet: IComponent
     {
         FaceType faceType;
         CharaName name;
@@ -40,7 +41,49 @@ namespace Soleil
         {
             name = _name;
             pictures = new CharacterPicture[(int)FaceType.size];
+            for (int i = 0; i < pictures.Length; i++)
+            {
+                var face = (FaceType)i;
+                var texID = CharacterPictureMap.GetTexture(name, face);
+                pictures[i] = new CharacterPicture(texID, _pos, null, false);
+            }
             Pos = _pos;
+        }
+
+        public void ChangeFace(FaceType faceTo)
+        {
+            pictures[(int)faceType].Quit();
+            faceType = faceTo;
+            pictures[(int)faceType].Call();
+        }
+
+        public void Call()
+        {
+            pictures[(int)faceType].Call();
+        }
+
+        public void Quit()
+        {
+            for (int i = 0; i < pictures.Length; i++)
+            {
+                pictures[i].Quit();
+            }
+        }
+
+        public void Update()
+        {
+            for (int i = 0; i < pictures.Length; i++)
+            {
+                pictures[i].Update();
+            }
+        }
+
+        public void Draw(Drawing d)
+        {
+            for (int i = 0; i < pictures.Length; i++)
+            {
+                pictures[i].Draw(d);
+            }
         }
     }
 }
