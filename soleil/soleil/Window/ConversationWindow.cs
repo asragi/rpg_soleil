b1882e1e6afd84ad9c4ee37aa5625e32b917d4df
asRagi <ragi-justaway@hotmail.co.jp>
2019-04-07 04:16:56 +0900

    Add Conversation Window class

---
+++ soleil/soleil/Window/ConversationWindow.cs
@@ -0,0 +1,14 @@
+﻿namespace Soleil
+{
+    class ConversationWindow: UIImage
+    {
+        public ConversationWindow()
+            :base(TextureID.MessageWindow,
+                 new Vector(0, 200),
+                 new Vector(0, 50),
+                 DepthID.MessageBack)
+        {
+
+        }
+    }
+}
