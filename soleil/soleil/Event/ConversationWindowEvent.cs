88c13f80b21cf3babd681e597cd9e60a4a9f022d
asRagi <ragi-justaway@hotmail.co.jp>
2019-04-07 04:27:31 +0900

    Add ConversationEvent

---
+++ soleil/soleil/Event/ConversationWindowEvent.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soleil.Event
+{
+    /// <summary>
+    /// メッセージウィンドウを生成する.
+    /// </summary>
+    class ConversationEvent: EventBase
+    {
+        string message;
+        ConversationWindow window;
+        public ConversationEvent(string _message)
+        {
+            message = _message;
+            window = new ConversationWindow();
+        }
+
+        public override void Start()
+        {
+            base.Start();
+            window.Call();
+        }
+
+        public override void Execute()
+        {
+            void ReactToInput()
+            {
+                window.Quit();
+                Next();
+            }
+
+            base.Execute();
+            if (KeyInput.GetKeyPush(Key.A)) ReactToInput();
+        }
+    }
+}
