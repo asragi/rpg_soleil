5c7c7f42cbdaac21a5b60a08ce8d12bb1a0ae04a
asRagi <ragi-justaway@hotmail.co.jp>
2019-05-19 22:23:37 +0900

    Add SkillHolder

---
+++ soleil/soleil/Skill/SkillHolder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soleil.Skill
+{
+    /// <summary>
+    /// キャラクターが持つスキルを管理するクラス
+    /// </summary>
+    class SkillHolder
+    {
+        bool[] learnedFlags;
+        public SkillHolder()
+        {
+            learnedFlags = new bool[(int)SkillID.size];
+        }
+
+        public bool HasSkill(SkillID id) => learnedFlags[(int)id];
+
+        public void LearnSkill(SkillID id) => learnedFlags[(int)id] = true;
+
+        public void ForgetSkill(SkillID id) => learnedFlags[(int)id] = false;
+    }
+}
