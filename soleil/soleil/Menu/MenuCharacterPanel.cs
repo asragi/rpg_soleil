3c989dd55b8b4369930bda3bcc33c8b4dffbbc42
asRagi <ragi-justaway@hotmail.co.jp>
2019-05-25 15:26:14 +0900

    Animation

--- soleil/soleil/Menu/MenuCharacterPanel.cs
+++ soleil/soleil/Menu/MenuCharacterPanel.cs
@@ -19,6 +19,7 @@ namespace Soleil.Menu
         UIImage faceImg;
         FontImage hpText, mpText, lvText;
         FontImage hpNumText, mpNumText, lvNumText;
+        readonly Vector posDiff = new Vector(50, 0);
 
         // ほんとは引数でキャラクターIDとかを渡してデータを参照する感じにしたいよね．
         public MenuCharacterPanel(Person p, Vector _pos, TextureID textureID)
@@ -28,21 +29,21 @@ namespace Soleil.Menu
             hp = p.Score.HPMAX;
             mp = p.Score.MPMAX;
             // Images
-            faceImg = new UIImage(textureID, pos + FaceImgPos, Vector.Zero, DepthID.MenuBottom, false, true, 0);
+            faceImg = new UIImage(textureID, pos + FaceImgPos, posDiff, DepthID.MenuBottom, false, true, 0);
             // hpmpImg
             var font = FontID.Yasashisa;
-            hpText = new FontImage(font, pos + HPPos, DepthID.MenuBottom, true, 0);
-            mpText = new FontImage(font, pos + HPPos + new Vector(0, SpaceHPMP), DepthID.MenuBottom, true, 0);
-            lvText = new FontImage(font, pos + HPPos + new Vector(30, -SpaceHPMP), DepthID.MenuBottom);
+            hpText = new FontImage(font, pos + HPPos, posDiff, DepthID.MenuBottom, true, 0);
+            mpText = new FontImage(font, pos + HPPos + new Vector(0, SpaceHPMP), posDiff, DepthID.MenuBottom, true, 0);
+            lvText = new FontImage(font, pos + HPPos + new Vector(30, -SpaceHPMP), posDiff, DepthID.MenuBottom);
             hpText.Text = "HP";
             mpText.Text = "MP";
             lvText.Text = "Lv";
             hpText.ActivateOutline(1);
             mpText.ActivateOutline(1);
             lvText.ActivateOutline(1);
-            hpNumText = new RightAlignText(font, pos + HPPos + new Vector(SpaceVal,0), Vector.Zero, DepthID.MenuBottom);
-            mpNumText = new RightAlignText(font, pos + HPPos + new Vector(SpaceVal, SpaceHPMP), Vector.Zero, DepthID.MenuBottom);
-            lvNumText = new RightAlignText(font, pos + HPPos + new Vector(SpaceVal, -SpaceHPMP), Vector.Zero, DepthID.MenuBottom);
+            hpNumText = new RightAlignText(font, pos + HPPos + new Vector(SpaceVal,0), posDiff, DepthID.MenuBottom);
+            mpNumText = new RightAlignText(font, pos + HPPos + new Vector(SpaceVal, SpaceHPMP), posDiff, DepthID.MenuBottom);
+            lvNumText = new RightAlignText(font, pos + HPPos + new Vector(SpaceVal, -SpaceHPMP), posDiff, DepthID.MenuBottom);
             hpNumText.Text = hp.ToString();
             mpNumText.Text = mp.ToString();
             lvNumText.Text = "3";
