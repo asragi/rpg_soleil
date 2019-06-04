7c38f17bdc310de658b92c586977f6435ad68505
asRagi <ragi-justaway@hotmail.co.jp>
2019-06-04 17:13:40 +0900

    Add UIGauge class

---
+++ soleil/soleil/UI/UIGauge.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework;
+using Soleil.Menu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soleil.UI
+{
+    class UIGauge: MenuComponent
+    {
+        public Vector Pos { get; set; }
+        int width, height;
+        double rate;
+        bool isRightSideEnd;
+
+        DepthID depth;
+
+        Vector drawsize;
+
+        public UIGauge(Vector pos, int _width, int _height, bool _isRightRideRnd, double initRate = 1.0)
+        {
+            Pos = pos;
+            (width, height, isRightSideEnd, rate) = (_width, _height, _isRightRideRnd, initRate);
+        }
+
+        public void Refresh()
+        {
+            drawsize = new Vector(width * rate, height);
+        }
+
+        public void Refresh(double _rate)
+        {
+            rate = _rate;
+            Refresh();
+        }
+
+        public override void Draw(Drawing d)
+        {
+            base.Draw(d);
+            d.DrawBox(Pos, drawsize, Color.White, depth);
+        }
+    }
+}
