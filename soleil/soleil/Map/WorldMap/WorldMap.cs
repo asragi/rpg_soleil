3909be1903ab863617429f6c4e02eef240b131aa
asRagi <ragi-justaway@hotmail.co.jp>
2019-06-06 23:52:03 +0900

    Add World Map Class

---
+++ soleil/soleil/Map/WorldMap/WorldMap.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soleil.Map.WorldMap
+{
+    class WorldMap
+    {
+        WorldPoint[] points;
+        public WorldMap()
+        {
+
+        }
+    }
+}
---
+++ soleil/soleil/Map/WorldMap/WorldPoint.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soleil.Map.WorldMap
+{
+    class WorldPoint
+    {
+        public readonly int ID; // csvの頂点のIDと一致させる．
+        public readonly Vector Pos; // ワールドマップ上の座標．
+
+    }
+}
