e77c460157e529d5ec6c3c0eb295c623010fe175
asRagi <ragi-justaway@hotmail.co.jp>
2019-06-16 18:37:46 +0900

    きもち

--- soleil/soleil/Item/ItemEffectData.cs
+++ soleil/soleil/Item/ItemEffectData.cs
@@ -18,16 +18,21 @@ namespace Soleil.Item
     /// </summary>
     static class ItemEffectData
     {
-        readonly static Dictionary<ItemID, System.Action<Person>> effectDictionary;
-
-        static ItemEffectData()
+        public static bool UseOnMenu(Person p, ItemID itemID)
         {
+            if (itemID == ItemID.Portion) return RecoverByRate(p, 30, 0);
+            return false;
 
-        }
+            bool RecoverByRate(Person target, double hpRate = 0, double mpRate = 0)
+            {
+                var hpRecoverRate = target.Score.HPMAX * (hpRate / 100);
+                var mpRecoverRate = target.Score.MPMAX * (mpRate / 100);
 
-        public static void UseOnMenu(Person p, ItemID itemID)
-        {
+                // Return false if using item is useless.
+                // ***** WIP *****
 
+                return true;
+            }
         }
 
         public static void UseOnMenu(PersonParty party, ItemID id)
