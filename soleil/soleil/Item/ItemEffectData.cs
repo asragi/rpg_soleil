eedc5110a0010bbd9ac930f9c8d633550a8d58a4
asRagi <ragi-justaway@hotmail.co.jp>
2019-06-16 18:25:35 +0900

    Add Item effect type

---
+++ soleil/soleil/Item/ItemEffectData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soleil.Item
+{
+    enum ItemEffectType
+    {
+        Recover,
+        Special,
+        size,
+    }
+
+    /// <summary>
+    /// アイテム使用効果の設定
+    /// </summary>
+    static class ItemEffectData
+    {
+        readonly static Dictionary<ItemID, System.Action<Person>> effectDictionary;
+
+        static ItemEffectData()
+        {
+
+        }
+
+        public static void UseOnMenu(Person p, ItemID itemID)
+        {
+
+        }
+
+        public static void UseOnMenu(PersonParty party, ItemID id)
+        {
+
+        }
+    }
+}
--- soleil/soleil/Menu/StatusSelect/ItemTargetSelect.cs
+++ soleil/soleil/Menu/StatusSelect/ItemTargetSelect.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Soleil.Item;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,10 +11,20 @@ namespace Soleil.Menu
     {
         ItemMenu itemMenu;
 
+        // 使用情報
+        ItemID id;
+        ItemList itemList;
+
         public ItemTargetSelect(ItemMenu _parent)
             :base(_parent)
         {
             itemMenu = _parent;
+        }
+
+        public void SetWillUsedItem(ItemID _id, ItemList bag)
+        {
+            id = _id;
+            itemList = bag;
         }
 
         public override void OnInputSubmit()
