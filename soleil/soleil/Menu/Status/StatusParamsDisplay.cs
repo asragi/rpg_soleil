7d30647f86595a2b77bf2fc7ecfca29ac06bda75
asRagi <ragi-justaway@hotmail.co.jp>
2019-06-21 23:45:33 +0900

    Paramsの位置

--- soleil/soleil/Menu/Status/StatusParamsDisplay.cs
+++ soleil/soleil/Menu/Status/StatusParamsDisplay.cs
@@ -8,7 +8,8 @@ namespace Soleil.Menu.Status
 {
     class StatusParamsDisplay : MenuComponent
     {
-        const int DiffY = 28;
+        const int DiffX = 132;
+        const int DiffY = 27;
         // もっとちゃんとしたところにおきたい
         readonly string[] Words = new[]
         {
@@ -17,7 +18,9 @@ namespace Soleil.Menu.Status
             "MAG",
             "SPD",
             "ATK",
-            "DEF"
+            "MAG",
+            "DEF",
+            "RES"
         };
 
         readonly int[] Para = new[] // 仮置き適当パラメータ
@@ -27,17 +30,22 @@ namespace Soleil.Menu.Status
             44,
             11,
             22,
+            36,
             18,
+            27,
         };
 
         TextWithVal[] texts;
 
         public StatusParamsDisplay(Vector pos)
         {
-            texts = new TextWithVal[6];
+            texts = new TextWithVal[8];
             for (int i = 0; i < texts.Length; i++)
             {
-                texts[i] = new TextWithVal(FontID.Touhaba, pos + new Vector(0,DiffY*i), 130, Words[i], Para[i]);
+                var xDiff = (i >= 4) ? DiffX : 0;
+                texts[i] = new TextWithVal(FontID.Yasashisa, pos + new Vector(xDiff, DiffY* (i % 4)), 116, Words[i], Para[i]);
+                texts[i].TextColor = ColorPalette.DarkBlue;
+                texts[i].ValColor = ColorPalette.DarkBlue;
             }
             AddComponents(texts);
         }
