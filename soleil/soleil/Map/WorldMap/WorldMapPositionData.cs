12335fb02f56a88228438af98f4f8faced4a4c68
asRagi <ragi-justaway@hotmail.co.jp>
2019-06-28 03:15:52 +0900

    カーソルをみやすく

--- soleil/soleil/Map/WorldMap/WorldMapPositionData.cs
+++ soleil/soleil/Map/WorldMap/WorldMapPositionData.cs
@@ -18,7 +18,7 @@ namespace Soleil.Map.WorldMap
         {
             positionDict = new Dictionary<WorldPointKey, Vector>();
             nameDict = new Dictionary<WorldPointKey, MapName>();
-            Set(WorldPointKey.Flare, MapName.Flare1, new Vector(0, 0));
+            Set(WorldPointKey.Flare, MapName.Flare1, new Vector(5593, 1336));
             Set(WorldPointKey.Somnia, MapName.Somnia1, new Vector(1183, 860));
 
             void Set(WorldPointKey key, MapName name, Vector pos)
