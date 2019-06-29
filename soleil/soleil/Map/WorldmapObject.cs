f6f363d488e642a111806e9ebc3c3d40364c6129
asRagi <ragi-justaway@hotmail.co.jp>
2019-06-30 03:13:22 +0900

    ワールドマップへ移動するイベント

---
+++ soleil/soleil/Map/WorldmapObject.cs
@@ -0,0 +1,36 @@
+﻿using Soleil.Event;
+using Soleil.Map.WorldMap;
+
+namespace Soleil.Map
+{
+    class WorldmapObject : MapEventObject
+    {
+        static readonly string[] options = new[]
+        {
+            "ワールドマップへ",
+            "キャンセル"
+        };
+
+        public WorldmapObject(
+            Vector pos, Vector size, WorldPointKey dest,
+            PersonParty pp, SceneManager sm, ObjectManager om, BoxManager bm
+            )
+            :base (pos, size, om, bm)
+        {
+            var wm = WindowManager.GetInstance();
+            EventSequence.SetEventSet(
+                new EventSet(
+                        new SelectWindowEvent(Pos, WindowTag.A, options)
+                    ),
+                new BoolEventBranch(EventSequence, () => wm.GetDecideIndex() == 0,
+                    new EventUnit[]{
+                        new ToWorldMapEvent(sm, pp, dest)
+                    },
+                    new EventUnit[]{
+                        new ChangeInputFocusEvent(InputFocus.Player)
+                    }
+                )
+            );
+        }
+    }
+}
