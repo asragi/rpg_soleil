67e1eb2b1de9e492920aae4560583bd5c8eb0c27
asRagi <ragi-justaway@hotmail.co.jp>
2019-06-30 03:01:21 +0900

    ワールドマップへ移動するイベントの追加

---
+++ soleil/soleil/Event/ToWorldMapEvent.cs
@@ -0,0 +1,43 @@
+﻿using Soleil.Map.WorldMap;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soleil.Event
+{
+    /// <summary>
+    /// ワールドマップに移動するイベントのクラス
+    /// </summary>
+    class ToWorldMapEvent: EventBase
+    {
+        SceneManager sceneManager;
+        PersonParty party;
+        WorldPointKey destinationKey;
+        Transition transition;
+
+        public ToWorldMapEvent(SceneManager sm, PersonParty pp, WorldPointKey dest)
+            : base()
+        {
+            sceneManager = sm;
+            party = pp;
+            destinationKey = dest;
+            transition = Transition.GetInstance();
+        }
+
+        public override void Start()
+        {
+            base.Start();
+            transition.SetMode(TransitionMode.FadeOut);
+        }
+
+        public override void Execute()
+        {
+            base.Execute();
+            // Transition終了でシーン切り替え
+            if (transition.GetTransitionMode() == TransitionMode.None)
+                new WorldMapScene(sceneManager, party, destinationKey);
+        }
+    }
+}
