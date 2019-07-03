4b08e038017f207ba8d962b58347c2b986528c2c
asRagi <ragi-justaway@hotmail.co.jp>
2019-07-03 13:58:19 +0900

    comment

--- soleil/soleil/Map/ICollideObject.cs
+++ soleil/soleil/Map/ICollideObject.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 
 namespace Soleil
 {
+    /// <summary>
+    /// 衝突判定を行うオブジェクトに対するインターフェイス．
+    /// </summary>
     interface ICollideObject
     {
         void OnCollisionEnter(CollideBox cb);
