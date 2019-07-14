c8e5b24c30c3e254099d0f3936dfae05ceaa5bc7
asRagi <ragi-justaway@hotmail.co.jp>
2019-07-15 00:43:51 +0900

    Add ShopStorageStore

---
+++ soleil/soleil/Event/Shop/ShopStorageStore.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soleil.Event
+{
+    /// <summary>
+    /// Shopごとの在庫状況を保持するクラス．
+    /// </summary>
+    class ShopStorageStore
+    {
+        private ShopStorage[] storages;
+        private static ShopStorageStore instance = new ShopStorageStore();
+        public ShopStorageStore GetInstance() => instance;
+        private ShopStorageStore()
+        {
+            storages = new ShopStorage[(int)ShopName.size];
+        }
+
+        public void Register(ShopName name, ShopStorage storage)
+        {
+            storages[(int)name] = storage;
+        }
+
+        /// <summary>
+        /// 登録されている情報を呼び出す．初回はnullを返す．
+        /// </summary>
+        public ShopStorage Get(ShopName name) => storages[(int)name];
+    }
+
+    enum ShopName
+    {
+        Accessary,
+        size
+    }
+}
