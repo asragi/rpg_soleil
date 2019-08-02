724983b2321db6ff44cd99a216c21ffa9ef7364a
asRagi <ragi-justaway@hotmail.co.jp>
2019-08-02 22:37:37 +0900

    Loadに対応するためPersonPartyを修正

--- soleil/soleil/Misc/PersonParty.cs
+++ soleil/soleil/Misc/PersonParty.cs
@@ -12,10 +12,43 @@ namespace Soleil
     {
         Person[] allCharacters;
 
+        /// <summary>
+        /// CreateNewParty
+        /// </summary>
         public PersonParty()
         {
-            allCharacters = new Person[(int)CharaName.size];
-            ReadData(); // debug
+            allCharacters = CreateNewParty();
+
+            Person[] CreateNewParty()
+            {
+                var result = new Person[(int)CharaName.size];
+                var a = new AbilityScore(72, 189, 5, 5, 44, 5);
+                var b = new AbilityScore(228, 121, 22, 18, 22, 20);
+                var c = new AbilityScore(164, 82, 13, 24, 12, 14);
+                var a_s = new SkillHolder();
+                var b_s = new SkillHolder(SkillID.PointFlare, SkillID.Thunder, SkillID.Headbutt);
+                var c_s = new SkillHolder(SkillID.Headbutt, SkillID.Sonicboom);
+                var a_m = new MagicLv(a_s);
+                for (int i = 0; i < (int)MagicCategory.size; i++)
+                {
+                    a_m.AddExp(100 - i * 10, (MagicCategory)i);
+                }
+                var b_m = new MagicLv(b_s);
+                var c_m = new MagicLv(c_s);
+                result[(int)CharaName.Lune] = new Person(CharaName.Lune, a, a_s, a_m);
+                result[(int)CharaName.Sunny] = new Person(CharaName.Sunny, b, b_s, b_m);
+                result[(int)CharaName.Tella] = new Person(CharaName.Tella, c, c_s, c_m);
+                return result;
+            }
+        }
+
+        /// <summary>
+        /// CreateFromSaveData
+        /// </summary>
+        /// <param name="people"></param>
+        public PersonParty(Person[] people)
+        {
+
         }
 
         public Person Get(CharaName name)
@@ -38,27 +71,6 @@ namespace Soleil
                 result.Add(target);
             }
             return result.ToArray();
-        }
-
-        public void ReadData()
-        {
-            // TODO: セーブデータから読み込み
-            var a = new AbilityScore(72, 189, 5, 5, 44, 5);
-            var b = new AbilityScore(228, 121, 22, 18, 22, 20);
-            var c = new AbilityScore(164, 82, 13, 24, 12, 14);
-            var a_s = new SkillHolder();
-            var b_s = new SkillHolder(SkillID.PointFlare, SkillID.Thunder, SkillID.Headbutt);
-            var c_s = new SkillHolder(SkillID.Headbutt, SkillID.Sonicboom);
-            var a_m = new MagicLv(a_s);
-            for (int i = 0; i < (int)MagicCategory.size; i++)
-            {
-                a_m.AddExp(100 - i * 10, (MagicCategory)i);
-            }
-            var b_m = new MagicLv(b_s);
-            var c_m = new MagicLv(c_s);
-            allCharacters[(int)CharaName.Lune] = new Person(CharaName.Lune, a, a_s, a_m);
-            allCharacters[(int)CharaName.Sunny] = new Person(CharaName.Sunny, b, b_s, b_m);
-            allCharacters[(int)CharaName.Tella] = new Person(CharaName.Tella, c, c_s, c_m);
         }
     }
 }
--- soleil/soleil/Misc/SaveLoad/SaveLoad.cs
+++ soleil/soleil/Misc/SaveLoad/SaveLoad.cs
@@ -15,6 +15,8 @@ namespace Soleil
     static class SaveLoad
     {
         private static readonly string FilePath = "./savedata";
+        private static SaveData data;
+
         public static void Save(PersonParty party)
         {
             var save = new Misc.SaveData(party);
@@ -30,12 +32,17 @@ namespace Soleil
         {
             // 読み込み
             string path = FilePath;
-            SaveData data;
             using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read))
             {
                 var bf = new BinaryFormatter();
                 data = (SaveData)bf.Deserialize(fs);
             }
+        }
+
+        // 以下Load用
+        public static PersonParty GetParty()
+        {
+            throw new NotImplementedException();
         }
     }
 }
