3691587edce9a72c63f3179d8901c797fe53a3e5
asRagi <ragi-justaway@hotmail.co.jp>
2019-08-09 04:17:57 +0900

    Add title scene

---
+++ soleil/soleil/Scene/TitleScene.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soleil
+{
+    class TitleScene: Scene
+    {
+        private static readonly Vector WindowPos = new Vector(128, 186);
+        private static readonly string[] Commands = new[]
+        {
+            "Load Game", "New Game", "Options", "Exit"
+        };
+
+        SelectableWindow selectWindow;
+        Image background;
+
+        public TitleScene(SceneManager sm)
+            : base(sm)
+        {
+            var transition = Transition.GetInstance();
+            transition.SetMode(TransitionMode.FadeIn);
+            selectWindow = new SelectableWindow(WindowPos, true, Commands);
+            selectWindow.Call();
+
+            // Graphics
+            background = new Image(TextureID.WhiteWindow, Vector.Zero, DepthID.BackGround, alpha: 1) { Color = ColorPalette.GlayBlue };
+        }
+
+        public override void Update()
+        {
+            base.Update();
+            background.Update();
+        }
+
+        public override void Draw(Drawing sb)
+        {
+            base.Draw(sb);
+            background.Draw(sb);
+        }
+    }
+}
