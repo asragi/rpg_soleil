7e7eff446e1c3b78c8cc04bc783414ea97754c3c
ano0406 <m20000406k@gmail.com>
2019-08-14 15:13:27 +0900

    訂正版

--- soleil/soleil/Menu/SkillMenu.cs
+++ soleil/soleil/Menu/SkillMenu.cs
@@ -26,6 +26,7 @@ namespace Soleil.Menu
 
         protected override SelectablePanel[] MakeAllPanels()
         {
+            //onMenuがtrueのものを上側に置く
             var availableSkillList = new List<SkillMenuPanel>();
             var unavailableSkillList = new List<SkillMenuPanel>();
             for (int i = 0; i < (int)SkillID.size; i++)
@@ -33,14 +34,13 @@ namespace Soleil.Menu
                 var id = (SkillID)i;
                 var _data = SkillDataBase.Get(id);
                 if (_data.AttackType != AttackType.Physical) continue;
-                if (skillHolder.HasSkill(id)) availableSkillList.Add(new SkillMenuPanel(_data, this, true));
+                if (!skillHolder.HasSkill(id)) continue;
+                if (_data.OnMenu) availableSkillList.Add(new SkillMenuPanel(_data, this, true));
                 else unavailableSkillList.Add(new SkillMenuPanel(_data, this, false));
             }
-            var avSkillArray = availableSkillList.ToArray();
-            var unavSkillArray = unavailableSkillList.ToArray();
-            var skillArray = new SelectablePanel[avSkillArray.Count() + unavSkillArray.Count()];
-            avSkillArray.CopyTo(skillArray, 0);
-            unavSkillArray.CopyTo(skillArray, avSkillArray.Count());
+            var skillArray = new SelectablePanel[availableSkillList.Count() + unavailableSkillList.Count()];
+            availableSkillList.ToArray().CopyTo(skillArray, 0);
+            unavailableSkillList.ToArray().CopyTo(skillArray, availableSkillList.Count());
             return skillArray;
         }
     }
