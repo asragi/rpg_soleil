8463dece01c548bb7d14b29e7649058b8f1a91fa
taiseiKMC <taisei@kmc.gr.jp>
2019-08-16 14:45:09 +0900

    Add AttackAttribution
    AttackAttrbutionを別ファイルへ分離

--- soleil/soleil/Battle/Action/Attack.cs
+++ soleil/soleil/Battle/Action/Attack.cs
@@ -7,32 +7,6 @@ using System.Threading.Tasks;
 namespace Soleil.Battle
 {
     using AttackFunc = Func<CharacterStatus, CharacterStatus, float>;
-    enum AttackAttribution
-    {
-        None = -1,
-        Beat,
-        Cut,
-        Thrust,
-        Fever,
-        Ice,
-        Electro,
-        size,
-    }
-
-    static class ExtendAttackAttribution
-    {
-        static readonly Dictionary<AttackAttribution, String> dict = new Dictionary<AttackAttribution, string>
-        {
-            {AttackAttribution.Beat, "打撃"},
-            {AttackAttribution.Cut, "斬撃"},
-            {AttackAttribution.Thrust, "弾丸"},
-            {AttackAttribution.Fever, "熱"},
-            {AttackAttribution.Ice, "冷気"},
-            {AttackAttribution.Electro, "電撃"},
-        };
-
-        public static String Name(this AttackAttribution att) => dict[att];
-    }
 
     /// <summary>
     /// ターンでの攻撃行動
