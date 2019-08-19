92ba2198ac9f4a4fbf33bfefec577aa741886618
asRagi <ragi@live.jp>
2019-08-19 21:37:32 +0900

    Add Dungeon Scene

---
+++ soleil/soleil/Scene/DungeonScene.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soleil
+{
+    class DungeonScene: Scene
+    {
+        public DungeonScene(SceneManager sm, PersonParty party)
+            :base(sm)
+        {
+
+        }
+    }
+}
