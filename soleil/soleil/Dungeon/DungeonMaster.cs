cb49577cc875261899f3609a5f637fb3dda0d824
asRagi <ragi@live.jp>
2019-08-20 20:45:23 +0900

    Add dungeon master

---
+++ soleil/soleil/Dungeon/DungeonMaster.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soleil.Dungeon
+{
+    /// <summary>
+    /// Dungeon処理に関するインスタンスを管理するクラス
+    /// </summary>
+    class DungeonMaster
+    {
+        public DungeonMaster(DungeonName name)
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw(Drawing d)
+        {
+
+        }
+    }
+}
--- soleil/soleil/Scene/DungeonScene.cs
+++ soleil/soleil/Scene/DungeonScene.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Soleil.Dungeon;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,10 +9,23 @@ namespace Soleil
 {
     class DungeonScene: Scene
     {
-        public DungeonScene(SceneManager sm, PersonParty party)
+        DungeonMaster master;
+        public DungeonScene(SceneManager sm, PersonParty party, DungeonName name)
             :base(sm)
         {
+            master = new DungeonMaster(name);
+        }
 
+        public override void Update()
+        {
+            base.Update();
+            master.Update();
+        }
+
+        public override void Draw(Drawing sb)
+        {
+            base.Draw(sb);
+            master.Draw(sb);
         }
     }
 }
