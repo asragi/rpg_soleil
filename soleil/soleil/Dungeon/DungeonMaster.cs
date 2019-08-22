cede4a85ddc0473522724cbcdc34c982bcc6bc2b
asRagi <ragi-justaway@hotmail.co.jp>
2019-08-22 17:17:36 +0900

    Add Input system

--- soleil/soleil/Dungeon/DungeonMaster.cs
+++ soleil/soleil/Dungeon/DungeonMaster.cs
@@ -1,19 +1,37 @@
 ﻿namespace Soleil.Dungeon
 {
+    enum DungeonMode
+    {
+        Init,
+        FirstWindow,
+        GoNext,
+        Search,
+        ReturnHome,
+    }
+
     /// <summary>
     /// Dungeon処理に関するインスタンスを管理するクラス
     /// </summary>
     class DungeonMaster
     {
         DungeonExecutor executor;
+        DungeonInput input;
+        FirstSelectWindow firstSelect;
         public DungeonMaster(DungeonName name)
         {
             executor = new DungeonExecutor(name);
+            firstSelect = new FirstSelectWindow();
+            firstSelect.Call();
+            input = new DungeonInput(this, firstSelect);
+            Mode = DungeonMode.Init;
         }
+
+        public DungeonMode Mode { get; set; }
 
         public void Update()
         {
             executor.Update();
+            input.Update();
         }
 
         public void Draw(Drawing d)
