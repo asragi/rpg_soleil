be6461167d706ad6e958f88fe7e0f1b5bd62f4bd
asRagi <ragi@live.jp>
2019-08-24 17:12:14 +0900

    Add return to home

---
+++ soleil/soleil/Dungeon/ReturnToHome.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soleil.Dungeon
+{
+    /// <summary>
+    /// ダンジョンの選択で入口に戻る際の処理
+    /// </summary>
+    class ReturnToHome
+    {
+        public ReturnToHome(PlayerObjectWrap player)
+        {
+
+        }
+    }
+}
