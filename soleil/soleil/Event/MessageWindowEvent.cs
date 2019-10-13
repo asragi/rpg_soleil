bc03a2d6643b821cddf911e8027e53c28047571c
asRagi <ragi-justaway@hotmail.co.jp>
2019-10-13 16:39:51 +0900

    Window Back Transparent

--- soleil/soleil/Event/MessageWindowEvent.cs
+++ soleil/soleil/Event/MessageWindowEvent.cs
@@ -1,4 +1,5 @@
-﻿using Soleil.Map;
+﻿using Microsoft.Xna.Framework;
+using Soleil.Map;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,6 +18,7 @@ namespace Soleil.Event
 
         MessageWindow messageW;
         bool isSpeech;
+        bool backImg;
 
         /// <summary>
         /// マップ上のオブジェクトを指定してメッセージウィンドウを出す．
@@ -25,25 +27,36 @@ namespace Soleil.Event
             : base(obj, MessageWindow.GetProperSize(MessageWindow.DefaultFont, _message))
         {
             message = _message;
+            isSpeech = true;
+            backImg = true;
         }
 
         /// <summary>
         /// マップ上の座標を指定してメッセージウィンドウを出す．
         /// </summary>
-        public MessageWindowEvent(Vector _pos, WindowTag _tag, string _message, bool speech = true, bool center = false)
+        public MessageWindowEvent(Vector _pos, WindowTag _tag, string _message, bool speech = true, bool center = false, bool _backImg = true)
             : base(_pos, MessageWindow.GetProperSize(MessageWindow.DefaultFont, _message), _tag, center)
         {
             isSpeech = speech;
             message = _message;
+            backImg = _backImg;
         }
 
         public override void Start()
         {
             base.Start();
+            // Window生成
             messageW = isSpeech ? new SpeechBubbleWindow(PosFunc(), Size)
                 : new MessageWindow(PosFunc(), Size);
+            // ウィンドウ生成
             messageW.SetMessage(message);
             messageW.Call();
+            // ウィンドウ背景がない場合の処理
+            if (!backImg)
+            {
+                messageW.Color = ColorPalette.AliceBlue;
+                messageW.BackImgAlpha = 0;
+            }
             // FocusをWindowに設定
             var mim = MapInputManager.GetInstance();
             mim.SetFocus(InputFocus.Window);
