cf92fdb7bfd558e02c44a830a7901907007868b1
asRagi <ragi-justaway@hotmail.co.jp>
2019-10-16 03:56:14 +0900

    add character event

--- soleil/soleil/Map/Map.cs
+++ soleil/soleil/Map/Map.cs
@@ -60,6 +60,7 @@ namespace Soleil.Map
             MapCameraManager = new MapCameraManager(player, cam);
             PictureHolder = new CharacterPictureHolder();
             ConversationSystem = new ConversationSystem(wm);
+            EventSequences = new EventSequence[10];
             SaveLoad.SaveRefs.NowMap = this;
         }
 
@@ -93,7 +94,7 @@ namespace Soleil.Map
         private void EventSequenceUpdate()
         {
             if (EventSequences == null) return;
-            for (int i = 0; i < EventSequences.Length; i++) EventSequences[i].Update();
+            for (int i = 0; i < EventSequences.Length; i++) EventSequences[i]?.Update();
         }
 
         public void SetPlayerPos(Vector pos) => om.SetPlayerPos(pos);
@@ -106,6 +107,7 @@ namespace Soleil.Map
             om.Draw(sb);
             PictureHolder.Draw(sb);
             ConversationSystem.Draw(sb);
+            EventSequences.ForEach2(e => e?.Draw(sb));
         }
     }
 }
