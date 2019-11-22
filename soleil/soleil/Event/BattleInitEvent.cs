83bf2c426bc6251ce6aef7b94a06f924112c211a
asRagi <ragi-justaway@hotmail.co.jp>
2019-11-22 16:26:28 +0900

    戦闘開始イベント

---
+++ soleil/soleil/Event/BattleInitEvent.cs
@@ -0,0 +1,61 @@
+﻿using Soleil.Battle;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soleil.Event
+{
+    class BattleInitEvent: EventBase
+    {
+        readonly BattleData data;
+        readonly SceneManager sceneManager;
+        readonly PersonParty party;
+        readonly Dictionary<int, System.Action> actions;
+        int frame;
+        public BattleInitEvent(BattleData _d, PersonParty p)
+        {
+            data = _d;
+            party = p;
+            sceneManager = SceneManager.GetInstance();
+            actions = new Dictionary<int, System.Action>();
+
+            actions.Add(1, FadeOut);
+            actions.Add(40, InitBattle);
+            actions.Add(45, FadeIn);
+            actions.Add(100, End);
+        }
+
+        public override void Execute()
+        {
+            base.Execute();
+            frame++;
+            if (actions.ContainsKey(frame)) actions[frame]();
+        }
+
+        private void InitBattle()
+        {
+            new TestBattleScene(sceneManager, party, data.Enemies);
+        }
+
+        private void End()
+        {
+            frame = 0;
+            Next();
+        }
+
+        private void FadeOut()
+        {
+            var transition = Transition.GetInstance();
+            transition.SetMode(TransitionMode.FadeOut);
+        }
+
+        private void FadeIn()
+        {
+            var transition = Transition.GetInstance();
+            transition.SetMode(TransitionMode.FadeIn);
+        }
+    }
+}
+
--- soleil/soleil/Map/Maps/Somnia/Objects/BlueGirl.cs
+++ soleil/soleil/Map/Maps/Somnia/Objects/BlueGirl.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Soleil.Battle;
 using Soleil.Event;
 
 namespace Soleil.Map.Maps.Somnia
 {
     class BlueGirl : MapCharacter
     {
-        public BlueGirl(Vector pos, ObjectManager om, BoxManager bm)
+        public BlueGirl(Vector pos, ObjectManager om, BoxManager bm, PersonParty party)
             : base("ciger", pos, null, om, bm)
         {
             AnimationData anim = new AnimationData(AnimationID.MobBlueGirl, new Vector(-5, -45), true, 8);
@@ -21,6 +22,17 @@ namespace Soleil.Map.Maps.Somnia
                 new MessageWindowEvent(this, "ここはすごく雰囲気悪いね"),
                 new MessageWindowEvent(this, "......"),
                 new MessageWindowEvent(this, "すっごくワクワクするね！！"),
+                new WaitEvent(20),
+                new MessageWindowEvent(this, "そうだ！ちょっとペットを連れてきてるんだけど，\nよかったら見ていかない？"),
+                new SelectWindowEvent(this, "はい", "いいえ"),
+                new BoolEventBranch(EventSequence, () => WindowManager.GetInstance().GetDecideIndex() == 0,
+                        new EventUnit[] {
+                            new BattleInitEvent(new BattleData(CharacterType.TestEnemy, CharacterType.TestEnemy), party)
+                        },
+                        new EventUnit[] {
+                            new MessageWindowEvent(this, "そっか")
+                        }
+                ),
                 new ChangeInputFocusEvent(InputFocus.Player)
             );
         }
