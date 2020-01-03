8c81fa8c3f78408c6bb96148363b79ebe63ef533
asRagi <ragi-justaway@hotmail.co.jp>
2020-01-04 08:32:28 +0900

    remove unused event

--- soleil/soleil/Map/Maps/Magistol/MagistolCol1.cs
+++ soleil/soleil/Map/Maps/Magistol/MagistolCol1.cs
@@ -52,24 +52,6 @@ namespace Soleil.Map
             new MapChangeObject((new Vector(280, 280), new Vector(144, 357)), MapName.MagistolShop, new Vector(665, 690), Direction.LU, om, bm, party, cam);
             new MapChangeObject((new Vector(1271, 1305), new Vector(1319, 1278)), MapName.MagistolRoom, new Vector(338, 400), Direction.RD, om, bm, party, cam);
             new WorldmapObject("tmp", new Vector(853, 400), new Vector(30, 30), WorldMap.WorldPointKey.Magistol, party, om, bm);
-
-            // debug
-            EventSequences = new Event.EventSequence[1];
-            EventSequences[0] = new Event.EventSequence(om.GetPlayer());
-            EventSequences[0].SetEventSet(
-                new Event.MessageWindowEvent(new Vector(550, 500), 0, "サニーが仲間になった！"),
-                new Event.ChangeInputFocusEvent(InputFocus.Player),
-                new Event.CharacterActivateEvent(Party, Misc.CharaName.Sunny, true)
-                );
-        }
-
-        protected override void Start()
-        {
-            base.Start();
-            if (!Party.Get(Misc.CharaName.Sunny).InParty)
-            {
-                EventSequences[0].StartEvent();
-            }
         }
     }
 }
