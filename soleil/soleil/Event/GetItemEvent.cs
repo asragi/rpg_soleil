a53e0ebaf4007b31881a515587a16f4e6e843f70
asRagi <ragi.zip@live.jp>
2020-01-18 22:38:32 +0900

    Add GetItemEvent

---
+++ soleil/soleil/Event/GetItemEvent.cs
@@ -0,0 +1,37 @@
+﻿using Soleil.Item;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soleil.Event
+{
+    class GetItemEvent: EventBase
+    {
+        private readonly ItemID itemID;
+        private readonly ToastMaster toastMaster;
+        private int num;
+        public GetItemEvent(ItemID id, ToastMaster tm, int _num = 1)
+        {
+            itemID = id;
+            toastMaster = tm;
+            num = _num;
+        }
+
+        public override void Execute()
+        {
+            base.Execute();
+            // display Toast
+            var itemData = ItemDataBase.Get(itemID);
+            ItemType type = itemData.Type;
+            TextureID icon = type.GetIcon();
+            string name = itemData.Name;
+            toastMaster.PopUpAlert(icon, name, num);
+            // Add Item To Player Bag
+            var itemBag = PlayerBaggage.GetInstance().Items;
+            itemBag.AddItem(itemID, num);
+            Next();
+        }
+    }
+}
