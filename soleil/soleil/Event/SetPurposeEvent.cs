84e645251da450b1984a89f63e2cad4e2ca26faa
asRagi <ragi.zip@live.jp>
2020-02-14 00:02:59 +0900

    Display Purpose Event

---
+++ soleil/soleil/Event/SetPurposeEvent.cs
@@ -0,0 +1,30 @@
+﻿using Soleil.Misc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soleil.Event
+{
+    /// <summary>
+    /// 次の行動のヒントの表示を変更するイベント
+    /// </summary>
+    class SetPurposeEvent: EventBase
+    {
+        readonly PurposeName purposeName;
+        public SetPurposeEvent(PurposeName pn)
+        {
+            purposeName = pn;
+        }
+
+        public override void Execute()
+        {
+            base.Execute();
+
+            var purposeHolder = PurposeHolder.Instance;
+            purposeHolder.SetPurpose(purposeName);
+            Next();
+        }
+    }
+}
