727b9099387fb8e1fa219ea5ea8a1af07c63e816
asRagi <ragi-justaway@hotmail.co.jp>
2020-02-16 01:18:33 +0900

    Add CharacterDisappearEvent

---
+++ soleil/soleil/Event/CharacterDisappearEvent.cs
@@ -0,0 +1,27 @@
+﻿using Soleil.Map;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soleil.Event
+{
+    class CharacterDisappearEvent: EventBase
+    {
+        readonly MapCharacter character;
+
+        public CharacterDisappearEvent(MapCharacter target)
+        {
+            character = target;
+        }
+
+        public override void Execute()
+        {
+            base.Execute();
+            character.Visible = false;
+            character.Exist = false;
+            Next();
+        }
+    }
+}
